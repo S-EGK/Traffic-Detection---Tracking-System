d80920e945d4b47e45309c61ea02c5b79467ed68
Srikanth Elkoori Ghantala Karnam <elkoorsh@mail.uc.edu>
2023-12-27 20:05:33 -0500

    Create repo

---
+++ train_yolov8n.py
@@ -0,0 +1,14 @@
+import torch
+from ultralytics import YOLO
+
+def main():
+    # Load the model
+    model = YOLO('yolov8n.yaml')  # build a new model from YAML
+    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+
+    # Train the model
+    results = model.train(data='dataset\data.yaml', epochs=100, imgsz=640, device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
+
+if __name__ == '__main__':
+    main()
